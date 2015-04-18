9b812f7eb2fc77073bbb09cc0449d62496cb93f8
Pavan Kumar <admin@technophihleshub.com>
2015-04-18 14:23:47 +0530

    Facebook SDK V4

---
+++ initialize.php
@@ -0,0 +1,7 @@
+<?php
+//Use app id,secret and redirect url
+$app_id = 'APP_ID';
+$app_secret = 'APP_SECRET';
+$redirect_url = 'http://localhost/facebookv4/fbconfig.php';
+$logout = 'http://localhost/facebook/logout/';
+?>
